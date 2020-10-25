690cb377f180e9679ac335acb8b8bba4ada80f00
Andrew0502 <agcmarcus@gmail.com>
2020-10-25 11:06:38 -0400

    Attempted fix on server.js.

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
-const PORT = 3000;
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
